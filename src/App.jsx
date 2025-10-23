b9a2c8a9afe5ca9b6f14ebc49fa96a5d2e6e018d
Jason <byronjason902@gmail.com>
2025-10-23 10:42:12 +0100

    feat: Add Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl text-indigo-300">Hello</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
